aa64ed6652e772a49a1f53adf579551852d5ddcd
Berkeley Churchill <berkeley@berkeleychurchill.com>
2019-03-12 00:42:04 -0800

    wrote an actual test

--- EFCacheContainsTest/CacheContainsTest.cs
+++ EFCacheContainsTest/CacheContainsTest.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
 using berkeleychurchill.CacheContains;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace berkeleychurchill.CacheContains.Test
 {
@@ -11,9 +13,18 @@ namespace berkeleychurchill.CacheContains.Test
         }
 
         [Test]
-        public void Test1()
+        public void ContainsWorks()
         {
-            Assert.Pass();
+            IEnumerable<int> myList = new List<int>() { 2, 4, 8 };
+            IQueryable<int> myStore = (new List<int>() { 1, 2, 3, 4, 5, 6, 7 }).AsQueryable();
+
+            var result = from i in myStore
+                         where myList.Contains(i)
+                         select i;
+
+            var expected = new List<int>() { 2, 4 };
+
+            Assert.AreEqual(expected, result.ToList());
         }
     }
 }
