af584731a2382f6c6ba7a0fb1139c5520fbd4582
Berkeley Churchill <berkeley@berkeleychurchill.com>
2019-03-12 00:32:32 -0800

    adding tests

---
+++ EFCacheContainsTest/CacheContainsTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using berkeleychurchill.CacheContains;
+
+namespace berkeleychurchill.CacheContains.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
